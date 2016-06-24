da25985ad0f1c2d7cc5cee943c5691ca869c9027
Moritz Moeller <mm@mxs.de>
2016-06-24 20:51:06 +0200

    running periph dump

--- utility/dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openocd -f interface/stlink-v2.cfg -f target/stm32f1x.cfg -c init -c targets \
-    -c "dump_image dump-4001000.bin 0x40010000 0x4000" \
-    -c "dump_image dump-4000000.bin 0x40000000 0x8000" \
-    -c 'shutdown'
-
